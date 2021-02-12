f90aec19e56026434ee94d8e9ba846915dfb2b63
didukhroma <70459354+didukhroma@users.noreply.github.com>
2021-02-12 12:01:30 +0200

    first work project

--- src/js/theme.js
+++ src/js/theme.js
@@ -8,14 +8,25 @@ const bodyRef = document.querySelector('body');
 const switcherRef = document.querySelector('#theme-switch-toggle ');
 //-----------------------------------------
 const themeLocalStorage = localStorage.getItem('theme');
-bodyRef.classList.add(Theme.LIGHT);
+if (themeLocalStorage === null) {
+  bodyRef.classList.add(Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+} else if (themeLocalStorage === 'dark-theme') {
+  bodyRef.classList.add(Theme.DARK);
+  switcherRef.checked = 'true';
+}
 //-----------------------------------------
 switcherRef.addEventListener('change', e => {
   if (bodyRef.classList.contains(Theme.LIGHT)) {
     bodyRef.classList.add(Theme.DARK);
     bodyRef.classList.remove(Theme.LIGHT);
+    switcherRef.checked = 'true';
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.DARK);
   } else {
     bodyRef.classList.add(Theme.LIGHT);
     bodyRef.classList.remove(Theme.DARK);
+    localStorage.removeItem('theme');
+    localStorage.setItem('theme', Theme.LIGHT);
   }
 });
