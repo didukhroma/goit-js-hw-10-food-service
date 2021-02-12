792b6f5c79716d57b42c8d4070d8a1924d12b62c
didukhroma <70459354+didukhroma@users.noreply.github.com>
2021-02-12 06:05:21 +0200

    add markup menu to html

---
+++ src/js/markupMenu.js
@@ -0,0 +1,8 @@
+//--------------------Import
+import menu from '../menu.json';
+import menuTemplate from '../template/menuItem.hbs';
+//--------------------Refs
+const listRef = document.querySelector('.js-menu');
+//--------------------Add markup to HTML
+const menuMarkup = menuTemplate(menu);
+listRef.insertAdjacentHTML('beforeend', menuMarkup);
